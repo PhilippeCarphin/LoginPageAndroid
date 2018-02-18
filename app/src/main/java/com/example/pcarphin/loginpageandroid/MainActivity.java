b7d90b582e41bc2c09ef80e9410f3baad63139ae
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-02-18 16:42:50 -0500

    Added two text fields and login button

--- app/src/main/java/com/example/pcarphin/loginpageandroid/MainActivity.java
+++ app/src/main/java/com/example/pcarphin/loginpageandroid/MainActivity.java
@@ -2,8 +2,26 @@ package com.example.pcarphin.loginpageandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.util.Log;
+import java.lang.CharSequence;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+     * This function prints the username and password to the console when the button is clicked
+     */
+    public void loginClicked(View view){
+
+        EditText usernameTextView = findViewById(R.id.loginName);
+        EditText passwordTextView = findViewById(R.id.loginPassword);
+
+        CharSequence username = usernameTextView.getText();
+        CharSequence password = passwordTextView.getText();
+
+        Log.i("Info", "Login was click: credentials : username=" + username + " , password=" + password);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
